ee22d07dcba4baf6c19c4fbc48b8cb3af5d888f6
RogerCaritj <43952278+RogerCaritj@users.noreply.github.com>
2018-10-10 13:51:42 +0200

    List with People at Home

--- app/src/main/java/com/firstapp/cbm/smarthome/MainActivity.java
+++ app/src/main/java/com/firstapp/cbm/smarthome/MainActivity.java
@@ -2,34 +2,65 @@ package com.firstapp.cbm.smarthome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mValueView;
-
     private Firebase mRef;
+
+    private ArrayList<String> mUsernames = new ArrayList<>();
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mValueView = (TextView) findViewById(R.id.valueView);
+        mListView = (ListView) findViewById(R.id.listView);
         mRef = new Firebase("https://iot-first-project-28e6a.firebaseio.com/at-home");
 
-        mRef.addValueEventListener(new ValueEventListener() {
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mUsernames);
+        mListView.setAdapter(arrayAdapter);
+
+        mRef.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
 
-                String value = dataSnapshot.getValue(String.class);
+                Map<String, String> map = dataSnapshot.getValue(Map.class);
 
-                mValueView.setText(value);
+                String value = map.get("id");
+                String name = map.get("name");
+
+
+                mUsernames.add(name);
+                arrayAdapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
             }
 
             @Override
@@ -37,5 +68,7 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+
     }
 }
