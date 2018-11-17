2cfb7c776ea75a21908c1ea4d2fcb279405a51a8
Carlos Bertomeu <carlos.bertomeu01@estudiant.upf.edu>
2018-11-17 14:05:10 +0100

    add icons and home page style

--- smart_school/HomeScreen.js
+++ smart_school/HomeScreen.js
@@ -3,8 +3,13 @@ import {
   View,
   Text,
   StyleSheet,
-  TouchableOpacity
+  TouchableOpacity,
+  Image,
+  Dimensions
 } from "react-native";
+import Icon from 'react-native-vector-icons/Ionicons'
+
+var { height, width } = Dimensions.get('window');
 
 class HomeScreen extends Component {
   static navigationOptions = {
@@ -13,28 +18,40 @@ class HomeScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={{ flex: 1, backgroundColor: 'tomato', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ flex: 1, backgroundColor: '#f79e51', width: '100%', height: '100%', justifyContent: 'center', alignItems: 'center', flexDirection: 'row' }}>
           <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Text>AA</Text>
+            <View style={{ justifyContent: 'center', alignItems: 'center', width: 0.3 * width, height: 0.3 * width, borderRadius: ((0.3 * width) / 2), borderWidth: 7, borderColor: 'white', marginTop: 0.07 * width }}>
+              <Image source={require('./assets/icon_tech.png')} style={{ width: 0.23 * width, height: 0.23 * width, borderRadius: ((0.23 * width) / 2), tintColor: 'white' }} />
+            </View>
           </View>
         </View>
-        <View style={{ flex: 1, backgroundColor: 'red', width: '100%', height: '100%' }}>
-          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Text>AA</Text>
+        <TouchableOpacity onPress={()=> {this.props.navigation.navigate('Subjects')}} style={{ flex: 1, backgroundColor: '#f78cb7', width: '100%', height: '100%', flexDirection: 'row' }}>
+          <View style={{ flex: 5, justifyContent: 'flex-start', alignItems: 'center', flexDirection: 'row', padding: 30 }}>
+            <Image source={require('./assets/icon_class.png')} style={{ width: 0.25 * width,resizeMode: 'contain', height: 0.25 * width, tintColor: 'white' }} />
+            <Text style={{ fontSize: 0.06 * width, paddingLeft: 0.06 * width, color: 'white', fontWeight: '700' }}>SUBJECTS</Text>
           </View>
-        </View>
-        <View style={{ flex: 1, backgroundColor: 'blue', width: '100%', height: '100%' }}>
-          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Text>AA</Text>
-
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Icon name="ios-arrow-forward" color="white" size={0.09 * width} />
           </View>
-        </View>
-        <View style={{ flex: 1, backgroundColor: 'yellow', width: '100%', height: '100%' }}>
-          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-            <Text>AA</Text>
-
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=> {this.props.navigation.navigate('Objects')}} style={{ flex: 1, backgroundColor: '#87e5f2', width: '100%', height: '100%', flexDirection: 'row' }}>
+          <View style={{ flex: 5, justifyContent: 'flex-start', alignItems: 'center', flexDirection: 'row', padding: 30 }}>
+            <Image source={require('./assets/logo.png')} style={{ width: 0.25 * width,resizeMode: 'contain', height: 0.25 * width, tintColor: 'white' }} />
+            <Text style={{ fontSize: 0.06 * width, paddingLeft: 0.06 * width, color: 'white', fontWeight: '700' }}>LABORATORIES</Text>
           </View>
-        </View>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Icon name="ios-arrow-forward" color="white" size={0.09 * width} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={{ flex: 1, backgroundColor: '#b0f96b', width: '100%', height: '100%', flexDirection: 'row' }}>
+          <View style={{ flex: 5, justifyContent: 'flex-start', alignItems: 'center', flexDirection: 'row', padding: 30 }}>
+            <Image source={require('./assets/info_icon.png')} style={{ width: 0.25 * width,resizeMode: 'contain', height: 0.25 * width, tintColor: 'white' }} />
+            <Text style={{ fontSize: 0.06 * width, paddingLeft: 0.06 * width, color: 'white', fontWeight: '700' }}>INFORMATION</Text>
+          </View>
+          <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Icon name="ios-arrow-forward" color="white" size={0.09 * width} />
+          </View>
+        </TouchableOpacity>
         {/* <View style={{justifyContent: 'center', alignItems:'center', margin: 20}}>
           <TouchableOpacity onPress={()=>{this.props.navigation.navigate('Subjects')}} style={{padding: 20, borderWidth:1}}>
             <Text>Subjects</Text>
